80886a6cf7aa55f16d7e78321c6813b4e79272fd
Mario <m.csirvent@alumnos.upm.es>
2021-05-28 14:18:15 +0200

    Codigo pedidos anteriores

--- cafeteria/main.c
+++ cafeteria/main.c
@@ -24,6 +24,26 @@ int main()
 	FILE *cf;
 	FILE *pf;
     char c;
+
+    FILE* pd;
+    FILE* pdr;
+
+    int opcion_bebidar;
+    int opcion_suplementor;
+    int opcion_comidar;
+
+    char bebida[50];
+    char suplemento[50];
+    char comida[50];
+    char x;
+
+    float precio_bebida[7];
+    float precio_suplemento[6];
+    float precio_comida[7];
+
+
+    float totalpedido;
+
  printf("Bienvenido a cafetsidi. Por favor, indique si quiere continuar como cliente o como administrador.\n");
  printf("1.- Cliente \t\t2.- Administrador\n");
  do
@@ -63,7 +83,7 @@ int main()
 					 {
 					 	printf("Usted no quiere suplemento\n");
 					 }
- 					
+
  					printf("¿Desea algo de comer?\n1.- Si\t\t2.- No");
  					scanf("%i",&quiere_comida);
  					if (quiere_comida == 1)
@@ -80,11 +100,11 @@ int main()
 					 tocapagar = preciototal (opcion_bebida, opcion_suplemento, opcion_comida);
 					 printf("A continuacion encontrara el resumen de su pedido:\nBebida: %i\nSuplemento: %i\nComida: %i\n A cobrar: %.2f\n", opcion_bebida, opcion_suplemento, opcion_comida, tocapagar);
 				break;
-					 
+
 					 //Aqui ira una funcion que imprima en pantalla el resumen del pedido y que calcule el precio total
-				
+
 				case 2:
-					 
+
                     pf=fopen("Menu.txt","r");
                     if(pf== NULL){
                     	printf("Error en la apertura del archivo\n");
@@ -99,7 +119,7 @@ int main()
                 break;
 			}
 			break;
-            
+
 		case 2:
 				cf=fopen("credenciales.txt","r");
 				if(cf==NULL)
@@ -108,10 +128,10 @@ int main()
 					return -1 ;
 				}
 				else
-				{			
+				{
 				while(fscanf(cf,"%c;%d;%d\n",&trabajadores[i].nombre,&trabajadores[i].usuario,&trabajadores[i].contra) !=EOF)
 					i++;
-				}			
+				}
 				fclose(cf);
 				printf("Ha accedido como un administrador.\n");
 				printf("A continuación debe inciar sesión.\n");
@@ -134,9 +154,9 @@ int main()
 			 	{
 			 		printf("Ha agotado los intentos\n");
 					printf("Hable con el encargado\n");
-					return 0;	
-				}		
-				comparador=0;	
+					return 0;
+				}
+				comparador=0;
 				intentos=0;
 				do
 				{
@@ -152,20 +172,211 @@ int main()
 				}while(intentos<3 && comparador==0);
 				if(comparador==0)
 				{
-					printf("Ha superado el numero de intentos\n"); 
+					printf("Ha superado el numero de intentos\n");
 					return 0;
           		}
-          		
+
 				printf("Menu de inicio como administrador\n");
 				return 0;
+
+				switch ()
+				{
+                case 1:
+                 pd = fopen("Pedidos.txt","a");
+					 if (pd==NULL)
+                        {
+                            printf("Error al abrir el fichero de escritura.\n");
+                            return -1;
+                        }
+                        else
+                        {
+                            fprintf(pd, "%i, %i, %i\n", opcion_bebida, opcion_suplemento, opcion_comida);
+                            printf("Pedido correctamente realizado\n");
+                            fclose(pd);
+
+precio_bebida[0] = 0;
+                    precio_bebida[1] = 3.50;
+                    precio_bebida[2] = 3.95;
+                    precio_bebida[3] = 4.95;
+                    precio_bebida[4] = 4.50;
+                    precio_bebida[5] = 4.50;
+                    precio_bebida[6] = 4.95;
+                    precio_bebida[7] = 3.95;
+
+                    precio_suplemento[0] = 0;
+                    precio_suplemento[1] = 0;
+                    precio_suplemento[2] = 0;
+                    precio_suplemento[3] = 0;
+                    precio_suplemento[4] = 0;
+                    precio_suplemento[5] = 0;
+                    precio_suplemento[6] = 0.5;
+
+                    precio_comida[0] = 0;
+                    precio_comida[1] = 6.50;
+                    precio_comida[2] = 6.50;
+                    precio_comida[3] = 7.95;
+                    precio_comida[4] = 3.00;
+                    precio_comida[5] = 5.90;
+                    precio_comida[6] = 4.50;
+                    precio_comida[7] = 1.00;
+
+                    pdr = fopen("Pedidos.txt", "r");
+
+                    if (pdr==NULL)
+                    {
+                        printf("Error al abrir el fichero de lectura.\n");
+                        return -1;
+                    }
+                    else
+                    {
+                        while(!feof(pdr) == 1)
+                        {
+                            fscanf(pdr, "%i, %i, %i", &opcion_bebidar, &opcion_suplementor, &opcion_comidar);
+
+                            totalpedido = 0;
+
+                            switch(opcion_bebidar)
+                            {
+                            case 1:
+
+                                strcpy(bebida, "Cafe solo");
+                                break;
+
+                            case 2:
+
+                                strcpy(bebida, "Cafe con leche");
+                                break;
+
+                            case 3:
+
+                                strcpy(bebida, "Cafe bombon");
+                                break;
+
+                            case 4:
+
+                                strcpy(bebida, "Capucchino");
+                                break;
+
+                            case 5:
+
+                               strcpy(bebida, "Chocolate");
+                               break;
+
+                            case 6:
+
+                                strcpy(bebida, "Chocolate con nata");
+                                break;
+
+                            case 7:
+
+                                strcpy(bebida, "Te");
+                                break;
+
+                            case 0:
+
+                                strcpy(bebida, "Sin bebida");
+                                break;
+
+                            default:
+                                break;
+                            }
+
+                            totalpedido = totalpedido + precio_bebida[opcion_bebidar];
+
+                            switch (opcion_suplementor)
+                            {
+                            case 1:
+
+                                strcpy(suplemento, "Azucar");
+                                break;
+
+                            case 2:
+                                strcpy(suplemento, "Miel");
+                                break;
+
+                            case 3:
+                                strcpy(suplemento, "Canela");
+                                break;
+
+                            case 4:
+                                strcpy(suplemento, "Leche condensada");
+                                break;
+
+                            case 5:
+                                strcpy(suplemento, "Hielo");
+                                break;
+
+                            case 6:
+                                strcpy(suplemento, "Sirope");
+                                break;
+
+                            case 0:
+                                strcpy(suplemento, "Sin suplemento");
+                                break;
+                            default:
+                                break;
+                            }
+
+                            totalpedido = totalpedido + precio_suplemento[opcion_suplementor];
+
+
+                            switch (opcion_comidar)
+                            {
+                            case 1:
+                                strcpy(comida, "Tostada con queso brie y mermelada de higos");
+                                break;
+
+                            case 2:
+                                strcpy(comida, "Tostada con aguacate y huevos revueltos");
+                                break;
+
+                            case 3:
+                                strcpy(comida, "Tostada con membrillo, queso, jamon y rucula");
+                                break;
+
+                            case 4:
+                                strcpy(comida, "Tostada con aceite y tomate");
+                                break;
+
+                            case 5:
+                                strcpy(comida, "Bowl de yogur");
+                                break;
+
+                            case 6:
+                                strcpy(comida, "Gofres con sirope y nata");
+                                break;
+
+                            case 7:
+                                strcpy(comida, "Bolleria");
+                                break;
+
+                            case 0:
+                                strcpy(comida, "Sin comida");
+                                break;
+                            default:
+                                break;
+                            }
+
+                            totalpedido = totalpedido + precio_comida[opcion_comidar];
+
+
+                            if (feof(pdr) == 0)
+
+                                 printf("bebida: %s\t suplemento: %s\t comida: %s\t importe del pedido: %.2f $\n", bebida, suplemento, comida, totalpedido);
+
+                        }
+                        fclose(pdr);
+
+                break;
+				}
 		break;
-		
+
  			default:
 				printf("hola\n");
-		
-			 
+
+
  			break;
- 	} 		
+ 	}
   }while (opcion_inicio <= 2);
 return 0;
 }
